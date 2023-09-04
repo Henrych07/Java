70847bf2134821b4828770137387e31ca8433c4e
Henrych07 <142981457+Henrych07@users.noreply.github.com>
2023-09-04 21:36:40 +0530

    Create switchcase.java

---
+++ switchcase.java
@@ -0,0 +1,189 @@
+// Java Program to Demonstrate Switch Case
+// with Multiple Cases Without Break Statements
+
+// Class
+public class GFG {
+
+	// main driver method
+	public static void main(String[] args)
+	{
+		int day = 2;
+		String dayType;
+		String dayString;
+
+		// Switch case
+		switch (day) {
+
+		// Case
+		case 1:
+			dayString = "Monday";
+			break;
+
+		// Case
+		case 2:
+			dayString = "Tuesday";
+			break;
+
+			// Case
+		case 3:
+			dayString = "Wednesday";
+			break;
+		case 4:
+			dayString = "Thursday";
+			break;
+		case 5:
+			dayString = "Friday";
+			break;
+		case 6:
+			dayString = "Saturday";
+			break;
+		case 7:
+			dayString = "Sunday";
+			break;
+		default:
+			dayString = "Invalid day";
+		}
+
+		switch (day) {
+			// Multiple cases without break statements
+
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			dayType = "Weekday";
+			break;
+		case 6:
+		case 7:
+			dayType = "Weekend";
+			break;
+
+		default:
+			dayType = "Invalid daytype";
+		}
+
+		System.out.println(dayString + " is a " + dayType);
+	}
+}
+
+
+
+
+// Java Program to Demonstrate
+// Nested Switch Case Statement
+
+// Class
+public class GFG {
+
+	// Main driver method
+	public static void main(String[] args)
+	{
+		// Custom input string
+		String Branch = "CSE";
+		int year = 2;
+
+		// Switch case
+		switch (year) {
+
+		// Case
+		case 1:
+			System.out.println(
+				"elective courses : Advance english, Algebra");
+
+			// Break statement to hault execution here
+			// itself if case is matched
+			break;
+
+			// Case
+		case 2:
+
+			// Switch inside a switch
+			// Nested Switch
+			switch (Branch) {
+
+			// Nested case
+			case "CSE":
+			case "CCE":
+				System.out.println(
+					"elective courses : Machine Learning, Big Data");
+				break;
+
+			// Case
+			case "ECE":
+				System.out.println(
+					"elective courses : Antenna Engineering");
+				break;
+
+				// default case
+				// It will execute if above cases does not
+				// execute
+			default:
+
+				// Print statement
+				System.out.println(
+					"Elective courses : Optimization");
+			}
+		}
+	}
+}
+
+
+
+// Java Program to Demonstrate
+// Nested Switch Case Statement
+
+// Class
+public class GFG {
+
+	// Main driver method
+	public static void main(String[] args)
+	{
+		// Custom input string
+		String Branch = "CSE";
+		int year = 2;
+
+		// Switch case
+		switch (year) {
+
+		// Case
+		case 1:
+			System.out.println(
+				"elective courses : Advance english, Algebra");
+
+			// Break statement to hault execution here
+			// itself if case is matched
+			break;
+
+			// Case
+		case 2:
+
+			// Switch inside a switch
+			// Nested Switch
+			switch (Branch) {
+
+			// Nested case
+			case "CSE":
+			case "CCE":
+				System.out.println(
+					"elective courses : Machine Learning, Big Data");
+				break;
+
+			// Case
+			case "ECE":
+				System.out.println(
+					"elective courses : Antenna Engineering");
+				break;
+
+				// default case
+				// It will execute if above cases does not
+				// execute
+			default:
+
+				// Print statement
+				System.out.println(
+					"Elective courses : Optimization");
+			}
+		}
+	}
+}
