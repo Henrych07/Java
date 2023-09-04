bc6cae80dbd71b9c3278f9d7dde89698b4be082f
Henrych07 <142981457+Henrych07@users.noreply.github.com>
2023-09-03 23:28:45 -0700

    Update Demoprimev2.java

--- Demoprimev2.java
+++ Demoprimev2.java
@@ -3,11 +3,11 @@ public class Demoprimev2
 {
     public static void main(String args[])
     {
-        int num,flag=0;
+        int num,i,flag=0;
         System.out.println("Enter an integer to check prime or not ");
         Scanner sc=new Scanner(System.in);
               num=sc.nextInt();
-        for(int i=2;i<=num/2;i++)
+        for(i=2;i<=num/2;i++)
         {
             if(num%i==0)
             {
