4ff099918c3b1bff1d40ea95bf7c32c955e34137
Henrych07 <142981457+Henrych07@users.noreply.github.com>
2023-09-25 12:21:18 +0530

    Create staticvariable.java

---
+++ staticvariable.java
@@ -0,0 +1,16 @@
+//Java programm to demonstrate static variable by using counter
+//25-09-2023
+//StaticVariableDemo1.java
+public class Counter{  
+int count=0;
+Counter(){  
+count++;  
+System.out.println(count);  
+}  
+  
+public static void main(String args[]){  
+Counter c1=new Counter();  
+Counter c2=new Counter();  
+Counter c3=new Counter();  
+}  
+}  
