cc0b09b8a023d0e57ec2c0b5009fc04011c2f434
Henrych07 <142981457+Henrych07@users.noreply.github.com>
2023-09-25 12:06:39 +0530

    Create staticblock2.java

---
+++ staticblock2.java
@@ -0,0 +1,11 @@
+//Write a java programm without using tha main method
+//25-09-2023
+//staticblock2.java
+public class A3
+{
+    static
+    {
+        System.out.println("Static block is invoked");
+        System.exit(0);
+    }
+}
