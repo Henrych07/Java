c8ff31b4bad511f66574982e176acfe3cab8bb07
Henrych07 <142981457+Henrych07@users.noreply.github.com>
2023-09-25 21:08:02 -0700

    Create Testthis1.java

---
+++ Testthis1.java
@@ -0,0 +1,22 @@
+//Java programme to demonstrate the importance of using this keyword
+//25-09-2023
+
+class Student{  
+int rollno;  
+String name;  
+float fee;  
+Student(int rollno,String name,float fee){  
+rollno=rollno;  
+name=name;  
+fee=fee;  
+}  
+void display(){System.out.println(rollno+" "+name+" "+fee);}  
+}  
+public class TestThis1{  
+public static void main(String args[]){  
+Student s1=new Student(4264,"Henry",45700);  
+Student s2=new Student(4204,"Ajay",45700);  
+s1.display();  
+s2.display();  
+}
+}  
